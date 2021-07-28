632a55355c5cf9b5915e425709338cc4041206a5
jjia <972931271@qq.com>
2021-07-28 20:25:57 +0200

    remove unnecessary code

--- kd_med/get_resnet3d.py
+++ kd_med/get_resnet3d.py
@@ -27,49 +27,31 @@ def generate_model(opt):
                 num_seg_classes=opt.n_seg_classes)
         elif opt.model_depth == 18:
             model = resnet.resnet18(
-                sample_input_W=opt.input_W,
-                sample_input_H=opt.input_H,
-                sample_input_D=opt.input_D,
                 shortcut_type=opt.resnet_shortcut,
                 no_cuda=opt.no_cuda,
                 num_seg_classes=opt.n_seg_classes)
         elif opt.model_depth == 34:
             model = resnet.resnet34(
-                sample_input_W=opt.input_W,
-                sample_input_H=opt.input_H,
-                sample_input_D=opt.input_D,
                 shortcut_type=opt.resnet_shortcut,
                 no_cuda=opt.no_cuda,
                 num_seg_classes=opt.n_seg_classes)
         elif opt.model_depth == 50:
             model = resnet.resnet50(
-                sample_input_W=opt.input_W,
-                sample_input_H=opt.input_H,
-                sample_input_D=opt.input_D,
                 shortcut_type=opt.resnet_shortcut,
                 no_cuda=opt.no_cuda,
                 num_seg_classes=opt.n_seg_classes)
         elif opt.model_depth == 101:
             model = resnet.resnet101(
-                sample_input_W=opt.input_W,
-                sample_input_H=opt.input_H,
-                sample_input_D=opt.input_D,
                 shortcut_type=opt.resnet_shortcut,
                 no_cuda=opt.no_cuda,
                 num_seg_classes=opt.n_seg_classes)
         elif opt.model_depth == 152:
             model = resnet.resnet152(
-                sample_input_W=opt.input_W,
-                sample_input_H=opt.input_H,
-                sample_input_D=opt.input_D,
                 shortcut_type=opt.resnet_shortcut,
                 no_cuda=opt.no_cuda,
                 num_seg_classes=opt.n_seg_classes)
         elif opt.model_depth == 200:
             model = resnet.resnet200(
-                sample_input_W=opt.input_W,
-                sample_input_H=opt.input_H,
-                sample_input_D=opt.input_D,
                 shortcut_type=opt.resnet_shortcut,
                 no_cuda=opt.no_cuda,
                 num_seg_classes=opt.n_seg_classes)

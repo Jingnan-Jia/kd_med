13570d6109c9284e5bc059db11fa82ee8f9d7622
jjia <972931271@qq.com>
2021-07-28 20:26:45 +0200

    add more test case

--- tests/test_GetEncSConv.py
+++ tests/test_GetEncSConv.py
@@ -13,21 +13,28 @@ from kd_med.kd_loss import GetEncSConv
 
 import numpy as np
 
-TEST_CASE_3d = [nn.Sequential(nn.Conv3d(1, 48, kernel_size=(3, 3, 3), stride=(2, 2, 2), padding=(3, 3, 3)),
-                              nn.Conv3d(48, 48, kernel_size=(3, 3, 3), stride=(2, 2, 2), padding=(3, 3, 3))),
-               nn.Conv3d(1, 32, kernel_size=(3, 3, 3), stride=(2, 2, 2), padding=(3, 3, 3)),
-                torch.ones((2, 1, 64, 64, 32)),
-               3]
+TEST_CASE_3d_1 = [nn.Sequential(nn.Conv3d(1, 48, kernel_size=(3, 3, 3), stride=(2, 2, 2), padding=(3, 3, 3)),
+                                nn.Conv3d(48, 48, kernel_size=(3, 3, 3), stride=(2, 2, 2), padding=(3, 3, 3))),
+                  nn.Conv3d(1, 32, kernel_size=(3, 3, 3), stride=(2, 2, 2), padding=(3, 3, 3)),
+                  torch.ones((2, 1, 64, 64, 32)),
+                  3]
+
+TEST_CASE_3d_2 = [nn.Sequential(nn.Conv3d(1, 48, kernel_size=(3, 3, 3), stride=(2, 2, 2), padding=(3, 3, 3)),
+                                nn.MaxPool3d(kernel_size=(2, 2, 2)),
+                                nn.Conv3d(48, 18, kernel_size=(3, 3, 3), stride=(2, 2, 2), padding=(3, 3, 3))),
+                  nn.Conv3d(1, 32, kernel_size=(3, 3, 3), stride=(2, 2, 2), padding=(3, 3, 3)),
+                  torch.ones((2, 1, 32, 16, 8)),
+                  3]
 
 TEST_CASE_2d = [nn.Sequential(nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(2, 2), padding=(3, 3)),
                               nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(2, 2), padding=(3, 3))),
-               nn.Conv2d(1, 16, kernel_size=(3, 3), stride=(2, 2), padding=(3, 3)),
+                nn.Conv2d(1, 16, kernel_size=(3, 3), stride=(2, 2), padding=(3, 3)),
                 torch.ones((2, 1, 64, 32)),
-               2]
+                2]
 
 
 class TestGetEncSConv(unittest.TestCase):
-    @parameterized.expand([TEST_CASE_3d, TEST_CASE_2d])
+    @parameterized.expand([TEST_CASE_3d_1, TEST_CASE_3d_2, TEST_CASE_2d])
     def test_GetEncSConv(self, enc_t, enc_s, input_img, dims):
         enc_s_and_conv1 = GetEncSConv().get(enc_t, enc_s, dims)
         enc_s_and_conv2 = GetEncSConv().get(enc_t, enc_s, dims)
@@ -38,7 +45,6 @@ class TestGetEncSConv(unittest.TestCase):
         self.assertIs(enc_s_and_conv1, enc_s_and_conv3)
 
         self.assertEqual(enc_s_and_conv1(input_img).shape, enc_t(input_img).shape)
-
 
 
 if __name__ == "__main__":
